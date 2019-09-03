8f461a263c2aa636b785dcf9590dc99af263f3a2
Garrick Chou <guitarkid35@gmail.com>
2019-09-03 18:28:32 +0800

    fix: pic random

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -26,14 +27,22 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int allBall = 1;
+
+  void ballRandom() {
+    setState(() {
+      allBall = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
         onPressed: () {
-          print('I got ball!');
+          ballRandom();
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$allBall.png'),
       ),
     );
   }
